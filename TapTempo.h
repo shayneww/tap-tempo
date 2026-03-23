ddadd4d3b3a447b6ec3f65ac22b9c4e4c7343f57
shayne <shaynemarinw@gmail.com>
2026-03-23 21:22:11 +0100

    More comments added, slight logic changes

--- TapTempo.h
+++ TapTempo.h
@@ -13,6 +13,5 @@ private:
 	std::vector<double> intervals;
 	std::chrono::time_point<std::chrono::steady_clock> lastTapTime;
 	bool isFirstTap;
-	const int AVERAGE = 16;
+	const int ROLLING_AVERAGE = 16;
 };
-
